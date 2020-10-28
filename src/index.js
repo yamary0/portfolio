65e6e37c2b985c732cbba4e07137aa61e1cdd70f
yamary0 <40788982+yamary0@users.noreply.github.com>
2020-10-28 17:20:43 +0900

    centering img tag in Work.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './global.scss';
-import Layout from './components/Layout'
-import History from './pages/History'
+import Layout from './components/Layout/Layout'
+import Work from './pages/Work/Work'
+// import History from './pages/History/History'
 
 
 ReactDOM.render(
   <Layout>
-    <History />
+    <Work />
+    {/* <History /> */}
   </Layout>,
   document.getElementById('root')
 );
